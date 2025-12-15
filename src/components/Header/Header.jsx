a11860b4b5f39c1dd07b618384688f61329b52b3
Emanuel Evangelista <emanu.vange@gmail.com>
2025-12-14 22:51:18 -0300

    feat: logo link Home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import logo from '../../assets/icon/logo2.png';
 import styles from './Header.module.css';
 
 const Header = () => (
   <header className={styles.header}>
-    <a href="/" className={styles.logo}>
+    <Link to="/" className={styles.logo}>
       <img className={styles.logoImage} src={logo} alt="Le Dôme Restaurant Logo" />
-    </a>
+    </Link>
   </header>
 );
 
