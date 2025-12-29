edf9c2a818d8761543a82dcb8c12b5cf78ced31f
Emanuel Evangelista <emanu.vange@gmail.com>
2025-12-29 16:24:55 -0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/le-dome-restaurant/" : "/",
+  base: "/le-dome-restaurant/",
   plugins: [react()],
   test: {
     environment: "jsdom",
