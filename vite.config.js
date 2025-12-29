6600769f1b3a478436e0717d750389c3997506a5
Emanuel Evangelista <emanu.vange@gmail.com>
2025-12-29 15:46:26 -0300

    fix name proyect

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/le-dome-restaurant/",
   plugins: [react()],
-  base: '/repo-restaurant-app/',
   test: {
     environment: "jsdom",
     globals: true,
